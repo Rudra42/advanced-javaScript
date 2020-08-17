efe427b501c9c15c4f8cdf734a9e7873632b138e
Rudra42 <36863915+Rudra42@users.noreply.github.com>
2020-08-18 02:39:13 +0600

    null undefined done

---
+++ null-vs-undefined.js
@@ -0,0 +1,13 @@
+let pakhi;
+console.log(pakhi);
+// value set nai tai undefied
+
+// function return na korle undefined ashbe
+
+// function er parameter pass na korle ta by Default undefined hobe
+
+// object er value set na korle ta undefined hobe
+
+// value undefined set kore dile ta undefined hobe
+
+// Null ==> explicitly set kore dile null. 
